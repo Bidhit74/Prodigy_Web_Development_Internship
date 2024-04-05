3caeebb7871dec0827819a1ccde79065a0f95690
Bidhit <bidhitkumar61@gmail.com>
2024-04-05 19:34:13 +0530

    Responsive and complete first task

--- Responsive Landing Page PRODIGY_WD_01/script.js
+++ Responsive Landing Page PRODIGY_WD_01/script.js
@@ -1,10 +1,8 @@
+const navbar = document.getElementById("navbar");
 window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
     if (window.scrollY > 0) {
         navbar.classList.add("scrolled");
-        console.log("scroll");
     } else {
         navbar.classList.remove("scrolled");
-        console.log("scroll1");
     }
 });
