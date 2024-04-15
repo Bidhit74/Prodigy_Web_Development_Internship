aba9d7a46ae281bd65c257915a47269fa781bd63
Bidhit <bidhitkumar61@gmail.com>
2024-04-15 12:58:27 +0530

    Complete Portfolio Work

---
+++ Personal Portfolio PRODIGY_WD_04/script.js
@@ -0,0 +1,13 @@
+let typeData = new Typed(".role", {
+    strings: [
+        "Full Stack Developer",
+        "Web Developer",
+        "Frontend Developer",
+        "Backend Developer",
+        "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
